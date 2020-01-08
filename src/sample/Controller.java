6a76e784a99503c5c8ac3d6b977a96e54fc279ee
SnowBeLock <karkardro@gmail.com>
2020-01-08 20:44:14 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.time.ZoneId;
+import java.util.TreeSet;
+
+public class Controller {
+    public ComboBox<String> departureTimeZone;
+    public ComboBox<String> destinationTimeZone;
+    public TextField departureDate;
+    public TextField departureTime;
+    public TextField result;
+
+    public void initialize(){
+        var timezones =new TreeSet<>(ZoneId.getAvailableZoneIds()) ;
+        departureTimeZone.getItems().addAll(timezones);
+        destinationTimeZone.getItems().addAll(timezones);
+    }
+    public void calculate() {
+        
+        var strDepartureDate=departureDate.getText();
+        result.setText(strDepartureDate);
+        System.out.println("POGA!"+ departureDate);
+    }
+}
